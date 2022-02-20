42d27e83e429c8d4a92eff11d1cf8fb7799f2ad7
zyb <vzmc12@gmail.com>
2022-02-20 23:22:37 +0900

    update mesh generator

--- ZYB_01/Assets/Scripts/SphereMeshGenerator.cs
+++ ZYB_01/Assets/Scripts/SphereMeshGenerator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,7 +11,9 @@ public class SphereMeshGenerator : MonoBehaviour
 {
     [SerializeField] private float _radius = 1;
     [SerializeField, Range(2, 50)] private int _weftCount = 5;
-    [SerializeField, Range(3, 50)] private int _warpCount = 10;
+    [SerializeField, Range(3, 100)] private int _warpCount = 10;
+    [SerializeField] private bool _createVerticesForEveryTriangles = false;
+    [SerializeField] private bool _smoothMesh = false;
     
     private Mesh _mesh;
     private MeshFilter _meshFilter;
@@ -18,10 +21,14 @@ public class SphereMeshGenerator : MonoBehaviour
     private float _tmpRadius = 0;
     private int _tmpWeftCount = 0;
     private int _tmpWarpCount = 0;
+    private bool _tmpVertexFlag = false;
+    private bool _tmpSmoothFlag = false;
 
     private void Start()
     {
+        _mesh = new Mesh();
         _meshFilter = GetComponent<MeshFilter>();
+        _meshFilter.mesh = _mesh;
     }
 
     private void Update()
@@ -29,13 +36,21 @@ public class SphereMeshGenerator : MonoBehaviour
         // 球体の情報が変わったら再生成する
         if (_tmpRadius.CompareTo(_radius) != 0
             || _tmpWeftCount.CompareTo(_weftCount) != 0
-            || _tmpWarpCount.CompareTo(_warpCount) != 0)
+            || _tmpWarpCount.CompareTo(_warpCount) != 0
+            || _tmpVertexFlag.CompareTo(_createVerticesForEveryTriangles) != 0)
         {
             _tmpRadius = _radius;
             _tmpWeftCount = _weftCount;
             _tmpWarpCount = _warpCount;
-            _mesh = GenerateSphereMesh(_radius, _weftCount, _warpCount);
-            _meshFilter.mesh = _mesh;
+            _tmpVertexFlag = _createVerticesForEveryTriangles;
+            GenerateSphereMesh(_radius, _weftCount, _warpCount, _createVerticesForEveryTriangles, ref _mesh);
+            SetMeshNormals(_smoothMesh, ref _mesh);
+        }
+
+        if (_tmpSmoothFlag != _smoothMesh)
+        {
+            _tmpSmoothFlag = _smoothMesh;
+            SetMeshNormals(_smoothMesh, ref _mesh);
         }
     }
 
@@ -67,15 +82,13 @@ public class SphereMeshGenerator : MonoBehaviour
     }
 
     // 球体メッシュを生成する
-    // 頂点位置ごとに1つの頂点を置く
-    private Mesh GenerateSphereMesh(float radius, int weftCount, int warpCount)
+    private void GenerateSphereMesh(float radius, int weftCount, int warpCount, bool createVertexForEveryTriangles, ref Mesh mesh)
     {
-        var vertices = CreateSphereVerticesPositionList(radius, weftCount, warpCount);
-        var topIndex = vertices.Count - 2;
-        var bottomIndex = vertices.Count - 1;
+        // まず頂点位置ごとに1つの頂点を置く
+        var tmpVertices = CreateSphereVerticesPositionList(radius, weftCount, warpCount);
         
-        var triangles = new List<int>();
         // 頂点リストから三角形の頂点の番号を三角形リストに追加する
+        var tmpTriangles= new List<int>();
         for (var i = 0; i < weftCount - 1; i++)
         {
             int start = i * warpCount;
@@ -88,37 +101,71 @@ public class SphereMeshGenerator : MonoBehaviour
                     int two = one < end - 1 ? one + 1 : start;
                     int three = two + warpCount;
                     int four = one + warpCount;
-                    triangles.AddRange(new List<int>{ one, two, three });
-                    triangles.AddRange(new List<int>{ one, three, four });
+                    tmpTriangles.AddRange(new List<int>{ one, two, three });
+                    tmpTriangles.AddRange(new List<int>{ one, three, four });
                 }
                 if (i == 0)
                 {
-                    int one = topIndex;
+                    int one = tmpVertices.Count - 2;
                     int two = j;
                     int three = two < end - 1 ? two + 1 : start;
-                    triangles.AddRange(new List<int>{ one, three, two });
+                    tmpTriangles.AddRange(new List<int>{ one, three, two });
                 }
                 if (i == weftCount - 2)
                 {
-                    int one = bottomIndex;
+                    int one = tmpVertices.Count - 1;
                     int two = j;
                     int three = two < end - 1 ? two + 1 : start;
-                    triangles.AddRange(new List<int>{ one, two, three });
+                    tmpTriangles.AddRange(new List<int>{ one, two, three });
                 }
             }
         }
 
-        return CreateMesh(vertices, triangles);
+        List<Vector3> vertices;
+        List<int> triangles;
+        if (createVertexForEveryTriangles)
+        {
+            // 三角形ごとに3つの頂点を生成する
+            vertices = new List<Vector3>();
+            triangles = new List<int>();
+            for (int i = 0; i < tmpTriangles.Count; i++)
+            {
+                var index = tmpTriangles[i];
+                vertices.Add(tmpVertices[index]);
+                triangles.Add(i);
+            }
+        }
+        else
+        {
+            // そのまま使う
+            vertices = tmpVertices;
+            triangles = tmpTriangles;
+        }
+
+        GenerateMesh(vertices, triangles, ref mesh);
     }
 
     // メッシュ作る
-    private Mesh CreateMesh(List<Vector3> vertices, List<int> triangles)
+    private void GenerateMesh(List<Vector3> vertices, List<int> triangles, ref Mesh mesh)
     {
-        var mesh = new Mesh();
+        mesh.Clear();
         mesh.SetVertices(vertices);
         mesh.SetTriangles(triangles, 0);
-        mesh.RecalculateNormals();
-        return mesh;
+    }
+
+    // メッシュをスムースにするかどうか
+    private void SetMeshNormals(bool smooth, ref Mesh mesh)
+    {
+        if (smooth)
+        {
+            var normals = new List<Vector3>();
+            mesh.GetVertices(normals);
+            mesh.SetNormals(normals);
+        }
+        else
+        {
+            mesh.RecalculateNormals();
+        }
     }
 
     // 生成されたメッシュを保存する
